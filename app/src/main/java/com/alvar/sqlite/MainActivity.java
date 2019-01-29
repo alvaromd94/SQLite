b9b6e0321ebcee57eb010b28b88692fbabf23e17
alvaromd94 <37107484+alvaromd94@users.noreply.github.com>
2019-01-29 11:32:30 +0100

    BD

--- app/src/main/java/com/alvar/sqlite/MainActivity.java
+++ app/src/main/java/com/alvar/sqlite/MainActivity.java
@@ -1,7 +1,9 @@
 package com.alvar.sqlite;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DB_SQLite db = new DB_SQLite(this);
+        SQLiteDatabase conn = db.getWritableDatabase();
+
+        conn.execSQL("INSERT INTO PRODUCTO (nombre, cantidad, seccion) VALUES ('uno', 123, 'monitor')");
+        conn.execSQL("INSERT INTO PRODUCTO (nombre, cantidad, seccion) VALUES ('dos', 234, 'monitor')");
+        conn.execSQL("INSERT INTO PRODUCTO (nombre, cantidad, seccion) VALUES ('tres', 345, 'raton')");
+        conn.execSQL("INSERT INTO PRODUCTO (nombre, cantidad, seccion) VALUES ('cuatro', 456, 'teclado')");
+
+        conn.close();
+
+
+    }
+
+    public void guardarProducto(View view) {
+    }
+
+    public void eliminarProducto(View view) {
+    }
+
+    public void modificarProducto(View view) {
+    }
+
+    public void buscarProducto(View view) {
+    }
+
+    public void listarProducto(View view) {
     }
 }
